76b80b11603ed3553ffe156eb5e3f66dd279f709
Raj Dubey <raj@rajlabs.in>
2024-09-15 20:52:06 +0530

    chore: use static constant val

--- src/main/java/in/rajlabs/EmailService/service/KafkaJsonConsumerService.java
+++ src/main/java/in/rajlabs/EmailService/service/KafkaJsonConsumerService.java
@@ -1,13 +1,14 @@
 package in.rajlabs.EmailService.service;
 
 import in.rajlabs.EmailService.dto.EmailerInfo;
+import in.rajlabs.EmailService.util.CONSTANTS;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 @Service
 public class KafkaJsonConsumerService {
 
-    @KafkaListener(topics = "SEND_EMAILS", groupId = "json-group")
+    @KafkaListener(topics = CONSTANTS.KEY_KAFKA_EMAIL_TOPIC, groupId = "json-group")
     public void consumeJson(EmailerInfo emailerInfo) {
         // Handle EmailerInfo JSON messages
         System.out.println("Received EmailerInfo: " + emailerInfo);
