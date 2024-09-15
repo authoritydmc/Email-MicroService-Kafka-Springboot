cd7c04ef2793f11feb634ab02a77e0a3f7b93975
Raj Dubey <raj@rajlabs.in>
2024-09-15 19:40:21 +0530

    fix : issue with dev and default profile not returning JavaMailSender bean and also setup the proper default smtp details

--- src/main/java/in/rajlabs/EmailService/config/MailConfig.java
+++ src/main/java/in/rajlabs/EmailService/config/MailConfig.java
@@ -1,8 +1,8 @@
 package in.rajlabs.EmailService.config;
 
 import jakarta.mail.internet.MimeMessage;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -13,49 +13,32 @@ import org.springframework.mail.SimpleMailMessage;
 
 import java.io.InputStream;
 import java.util.Properties;
-
+@Slf4j
 @Configuration
 public class MailConfig {
 
-    private static final Logger LOGGER = LoggerFactory.getLogger(MailConfig.class);
+
 
     @Bean
     @Profile("default")
-    public JavaMailSender defaultJavaMailSender() {
-        return new JavaMailSender() {
-            @Override
-            public MimeMessage createMimeMessage() {
-                return null;
-            }
+    public JavaMailSender defaultMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp.gmail.com");
+        mailSender.setPort(587);
 
-            @Override
-            public MimeMessage createMimeMessage(InputStream contentStream) throws MailException {
-                return null;
-            }
+        String emailUsername = System.getenv("EMAIL_USERNAME");
+        String emailPassword = System.getenv("EMAIL_PASSWORD");
+        mailSender.setUsername(emailUsername != null ? emailUsername : "default-email@zohomail.com");
+        mailSender.setPassword(emailPassword != null ? emailPassword : "default-password");
+//        log.info("Set username "+mailSender.getUsername());
+//        log.info("Password"+mailSender.getPassword());
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.debug", "false");
 
-            @Override
-            public void send(MimeMessage... mimeMessages) throws MailException {
-
-            }
-
-            @Override
-            public void send(SimpleMailMessage simpleMessage) {
-                // Log the email details for testing
-                System.out.println("Default environment email sending:");
-                System.out.println("To: " + simpleMessage.getTo());
-                System.out.println("Subject: " + simpleMessage.getSubject());
-                System.out.println("Body: " + simpleMessage.getText());
-                System.out.println("Default environment: Email sending simulated!");
-            }
-
-            @Override
-            public void send(SimpleMailMessage... simpleMessages) {
-                for (SimpleMailMessage message : simpleMessages) {
-                    send(message);
-                }
-            }
-
-        };
+        return mailSender;
     }
 
     @Bean
@@ -79,11 +62,11 @@ public class MailConfig {
 
             @Override
             public void send(SimpleMailMessage simpleMessage) {
-                LOGGER.info("Development environment email sending:");
-                LOGGER.info("To: {}", (Object) simpleMessage.getTo());
-                LOGGER.info("Subject: {}", simpleMessage.getSubject());
-                LOGGER.info("Body: {}", simpleMessage.getText());
-                LOGGER.info("Development environment: Email sending mocked!");
+                log.info("Development environment email sending:");
+                log.info("To: {}", (Object) simpleMessage.getTo());
+                log.info("Subject: {}", simpleMessage.getSubject());
+                log.info("Body: {}", simpleMessage.getText());
+                log.info("Development environment: Email sending mocked!");
             }
 
             @Override
