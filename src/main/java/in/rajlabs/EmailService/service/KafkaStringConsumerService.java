f619d9b1d0c33db5428d3ac65cbdaff9c9da1e95
Raj Dubey <raj@rajlabs.in>
2024-09-15 20:52:06 +0530

    chore: use static constant val

--- src/main/java/in/rajlabs/EmailService/service/KafkaStringConsumerService.java
+++ src/main/java/in/rajlabs/EmailService/service/KafkaStringConsumerService.java
@@ -1,12 +1,13 @@
 package in.rajlabs.EmailService.service;
 
+import in.rajlabs.EmailService.util.CONSTANTS;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 @Service
 public class KafkaStringConsumerService {
 
-    @KafkaListener(topics = "SEND_EMAILS", groupId = "string-group")
+    @KafkaListener(topics = CONSTANTS.KEY_KAFKA_EMAIL_TOPIC, groupId = "string-group")
     public void consumeString(String message) {
         // Handle string messages
         System.out.println("Received string message: " + message);
